5d64ee43c3721829f22b8517eced4c59b1f02818
runfawu <micheal@micheal-2.local>
2015-01-27 11:36:29 +0800

    项目创建

---
+++ SmartHome/SmartHome/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SmartHome
+//
+//  Created by micheal on 15/1/27.
+//  Copyright (c) 2015年 renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
